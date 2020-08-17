04b4eb566433ad2e1b9fd6e640b1c19b4419775d
Kirill Ivanov <puaktopblm@yandex.ru>
2020-08-17 19:36:08 +0700

    внес исправления по части синтаксиса, чтобы все заработало. я не смог разобраться как автор планировал запускать код, поэтому внес свои правки.

---
+++ exaple.py
@@ -0,0 +1,18 @@
+import fins
+import udp
+import time
+
+fins_instance = udp.UDPFinsConnection()
+fins_instance.connect('172.16.201.201')
+fins_instance.dest_node_add=1
+fins_instance.srce_node_add=25
+
+for i in range(2):
+    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00',b'\x00\x1f',1)
+    mem_area = fins_instance.memory_area_read(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00')
+    print(mem_area)
+    time.sleep(1)
+    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00',b'\x00\x00',1)
+    mem_area = fins_instance.memory_area_read(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00')
+    print(mem_area)
+    time.sleep(1)
