ab01930a753ac862487f08f338e2d34cce21b314
Kirill Ivanov <puaktopblm@yandex.ru>
2020-08-18 17:45:30 +0700

    Переключается только один бит - дискретный выход номер три

--- exaple.py
+++ exaple.py
@@ -8,11 +8,11 @@ fins_instance.dest_node_add=1
 fins_instance.srce_node_add=25
 
 for i in range(2):
-    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00',b'\x00\x1f',1)
+    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_BIT,b'\x00\x64\x03',b'\x01',1)
     mem_area = fins_instance.memory_area_read(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00')
     print(mem_area)
     time.sleep(1)
-    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00',b'\x00\x00',1)
+    fins_instance.memory_area_write(fins.FinsPLCMemoryAreas().CIO_BIT,b'\x00\x64\x03',b'\x00',1)
     mem_area = fins_instance.memory_area_read(fins.FinsPLCMemoryAreas().CIO_WORD,b'\x00\x64\x00')
     print(mem_area)
     time.sleep(1)
